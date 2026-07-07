484234a34c7c7dc38f23c75812d69dd432afdfe1
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2026-07-07 22:51:15 +0100

    Change no-explicit-any ESLint rule from off to warn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,7 @@ export default [
     rules: {
       ...tseslintPlugin.configs.recommended.rules,
       "@typescript-eslint/no-unused-vars": ["warn"],
-      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
       "no-undef": "off" // TypeScript handles this
     },
   },
